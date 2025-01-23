58e69d2870a8d2f2ac66e1516b83777fde895f67
QuickScythe <QuickScythe@gmail.com>
2025-01-23 00:36:40 -0600

    debug code 2

--- src/main/java/me/quickscythe/quipt/api/events/EventHandler.java
+++ src/main/java/me/quickscythe/quipt/api/events/EventHandler.java
@@ -21,7 +21,7 @@ public class EventHandler {
 
     public void handle(QuiptEvent event){
         for(Listener listener : listeners){
-            System.out.println("Checking listener: " + listener.getClass().getName() + " for event: " + event.getClass().getName());
+            System.out.println("Checking listener: " + listener.getClass().getName() + " for event: " + event.listener().getName());
             if(event.listener().equals(listener.getClass())){
                 System.out.println("Processing event: " + event.getClass().getName() + " with listener: " + listener.getClass().getName());
                 listener.process(event);
