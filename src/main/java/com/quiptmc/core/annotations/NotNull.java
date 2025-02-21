cc2fd582b945af508ae231266319a8b16d4ab807
QuickScythe <QuickScythe@gmail.com>
2025-02-21 01:17:39 -0600

    added NotNull annotation

---
+++ src/main/java/com/quiptmc/core/annotations/NotNull.java
@@ -0,0 +1,10 @@
+package com.quiptmc.core.annotations;
+
+import java.lang.annotation.*;
+
+@Documented
+@Retention(RetentionPolicy.CLASS)
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE})
+public @interface NotNull {
+    String value() default "";
+}
